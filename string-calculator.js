c258d48d5ce73afa7c788534ba3de2722de89dde
Michael Pascuzzi <gatorpazz@gmail.com>
2015-05-08 12:29:28 -0400

    Update string-calculator.js
    Created toEnglish and plus functions

--- string-calculator.js
+++ string-calculator.js
@@ -1,1 +1,36 @@
-String
+var expect = require('chai').expect;
+
+function toEnglish(word){
+  if ( word === 'zero' ) {
+    return 0;
+  }
+  if ( word === 'one' ) {
+    return 1;
+  }
+  if ( word === 'two' ) {
+    return 2;
+  }
+  if ( word === 'three' ) {
+    return 3;
+  }
+  if ( word === 'four' ) {
+    return 4;
+  }
+  if ( word === 'five' ) {
+    return 5;
+  }
+  if ( word === 'six' ) {
+    return 6;
+  }
+  if ( word === 'seven' ) {
+    return 7;
+  }
+  if ( word === 'eight' ) {
+    return 8;
+  }
+
+function plus(x, y){
+  var a = toEnglish(x);
+  var b = toEnglish(y);
+  return a + b;
+}
