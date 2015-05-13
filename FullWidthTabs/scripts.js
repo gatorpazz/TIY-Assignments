a79e526c57469d336dd26f1204f256a1276d6a70
Michael Pascuzzi <gatorpazz@gmail.com>
2015-05-13 05:19:53 -0400

    Got tabs to click with function, working JS code

--- FullWidthTabs/scripts.js
+++ FullWidthTabs/scripts.js
@@ -1,1 +1,15 @@
-alert("I work in the html");
+//alert("I work in the html");
+var tabElements = document.querySelectorAll('a');
+var panelElements = document.querySelectorAll('section');
+
+_.forEach(tabElements, function(element, index, tabElements){
+    element.addEventListener('click', function(){
+      element.parentElement.classList.toggle('tab-current');
+    });
+});
+
+_.forEach(tabElements, function(element, index, panelElements){
+    element.addEventListener('click', function(){
+      element.classList.toggle('content-current');
+    });
+});
