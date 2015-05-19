a5a8dad5d4fb2e84a47c4e257782db2dee9dba99
Michael Pascuzzi <gatorpazz@gmail.com>
2015-05-19 04:03:19 -0400

    Made the javascript work to switch between views and altered the html to refer to it

--- ViewModeSwitch/scripts.js
+++ ViewModeSwitch/scripts.js
@@ -1,39 +1,17 @@
-/**
- * cbpViewModeSwitch.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2013, Codrops
- * http://www.codrops.com
- */
-(function() {
+var container = document.getElementById('cbp-vm');
+var gridSwitch = document.querySelector('a.cbp-vm-grid');
+var listSwitch = document.querySelector('a.cbp-vm-list');
 
-	var container = document.getElementById( 'cbp-vm' ),
-		optionSwitch = Array.prototype.slice.call( container.querySelectorAll( 'div.cbp-vm-options > a' ) );
+gridSwitch.addEventListener('click', function(){
+	gridSwitch.classList.toggle('cbp-vm-selected', true);
+	listSwitch.classList.toggle('cbp-vm-selected', false);
+	event.target.parentNode.parentNode.classList.toggle('cbp-vm-view-grid', true);
+	event.target.parentNode.parentNode.classList.toggle('cbp-vm-view-list', false);
+})
 
-	function init() {
-		optionSwitch.forEach( function( el, i ) {
-			el.addEventListener( 'click', function( ev ) {
-				ev.preventDefault();
-				_switch( this );
-			}, false );
-		} );
-	}
-
-	function _switch( opt ) {
-		// remove other view classes and any any selected option
-		optionSwitch.forEach(function(el) { 
-			classie.remove( container, el.getAttribute( 'data-view' ) );
-			classie.remove( el, 'cbp-vm-selected' );
-		});
-		// add the view class for this option
-		classie.add( container, opt.getAttribute( 'data-view' ) );
-		// this option stays selected
-		classie.add( opt, 'cbp-vm-selected' );
-	}
-
-	init();
-
-})();
+listSwitch.addEventListener('click', function(){
+	listSwitch.classList.toggle('cbp-vm-selected', true);
+	gridSwitch.classList.toggle('cbp-vm-selected', false);
+	event.target.parentNode.parentNode.classList.toggle('cbp-vm-view-list', true);
+	event.target.parentNode.parentNode.classList.toggle('cbp-vm-view-grid', false);
+})
