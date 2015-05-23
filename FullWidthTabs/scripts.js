a5dbdd9df6fbc6955e203a8d96b93f61a0bdd46b
Michael Pascuzzi <gatorpazz@gmail.com>
2015-05-23 01:43:22 -0400

    made tabs clickable and content switch, styled tabs a bit

--- FullWidthTabs/scripts.js
+++ FullWidthTabs/scripts.js
@@ -12,11 +12,41 @@
   $('img').attr('src', randoImage);
 
   var tabList = document.querySelector('nav');
-  var tabs = tabList.querySelectorAll('a')
-  _.for
+  var tabs = tabList.querySelectorAll('a');
 
-  $('a').click(function(){
+  $('a.tab1').click(function(){
+    $('section').removeClass('content-current');
+    $('#section-1').addClass('content-current');
+    $('a').removeClass('tab-current');
+    $('a.tab1').addClass('tab-current');
+  });
+
+  $('a.tab2').click(function(){
+    $('section').removeClass('content-current');
+    $('#section-2').addClass('content-current');
+    $('a').removeClass('tab-current');
+    $('a.tab2').addClass('tab-current');
+  });
+
+  $('a.tab3').click(function(){
     $('section').removeClass('content-current')
+    $('#section-3').addClass('content-current')
+    $('a').removeClass('tab-current');
+    $('a.tab3').addClass('tab-current');
+  });
+
+  $('a.tab4').click(function(){
+    $('section').removeClass('content-current')
+    $('#section-4').addClass('content-current')
+    $('a').removeClass('tab-current');
+    $('a.tab4').addClass('tab-current');
+  });
+
+  $('a.tab5').click(function(){
+    $('section').removeClass('content-current')
+    $('#section-5').addClass('content-current')
+    $('a').removeClass('tab-current');
+    $('a.tab5').addClass('tab-current');
   });
 
 })(window);
