49901732ee6a4f00fe851b862692659f1190ed4d
Michael Pascuzzi <gatorpazz@gmail.com>
2015-06-01 10:21:20 -0400

    Everything good

--- ViewModeSwitch/scripts.js
+++ ViewModeSwitch/scripts.js
@@ -15,3 +15,17 @@ listSwitch.addEventListener('click', function(){
 	event.target.parentNode.parentNode.classList.toggle('cbp-vm-view-list', true);
 	event.target.parentNode.parentNode.classList.toggle('cbp-vm-view-grid', false);
 })
+
+// $gridSwitch.click(function() {
+// 	$(this).toggle('cbp-vm-selected', true);
+// 	$listSwitch.toggle('cbp-vm-selected', false);
+// 	$('cbp-vm').toggle('cbp-vm-view-grid', true);
+// 	$('cbp-vm').toggle('cbp-vm-view-list', false);
+// });
+//
+// $listSwitch.click(function() {
+// 	$(this).toggle('cbp-vm-selected', true);
+// 	$gridSwitch.toggle('cbp-vm-selected', false);
+// 	$('cbp-vm').toggle('cbp-vm-view-list', true);
+// 	$('cbp-vm').toggle('cbp-vm-view-grid', false);
+// })
